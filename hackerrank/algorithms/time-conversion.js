1576331af3e88faebb9778e5b44f597f560bdbe9
Renan Andreolla <52181385+renanandreola@users.noreply.github.com>
2024-11-25 12:21:12 -0300

    feat: create time-conversion problem

---
+++ hackerrank/algorithms/time-conversion.js
@@ -0,0 +1,62 @@
+// https://www.hackerrank.com/challenges/one-week-preparation-kit-time-conversion/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+    const period = s.slice(-2);
+    let [hours, minutes, seconds] = s.slice(0, -2).split(":");
+    
+    if (period === "AM") {
+        if (hours === "12") {
+            hours = "00";
+        }
+    } else {
+        if (hours !== "12") {
+            hours = String(parseInt(hours, 10) + 12);
+        }
+    }
+    
+    // Combine the components into 24-hour format
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = timeConversion(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
